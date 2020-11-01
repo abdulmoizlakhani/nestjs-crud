32e39ba01ca8a272eacfb6a6ccdb8f310e6aac6e
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2020-11-01 15:33:10 +0500

    created CRUD operations of task management app without database

---
+++ nestjs-task-management/src/tasks/tasks.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task, TaskStatus } from './task.model';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+
+    if (status) {
+      tasks = tasks.filter(task => task.status === status);
+    }
+
+    if (search) {
+      tasks = tasks.filter(task => (task.title.includes(search) || task.description.includes(search)));
+    }
+
+    return tasks;
+  }
+
+  getTaskById(id: string): Task {
+    const [, task] = this.findTask(id);
+    return task;
+  }
+
+  deleteTask(id: string): Task {
+    const [taskIndex, task] = this.findTask(id);
+    this.tasks.splice(taskIndex, 1);
+    return task;
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    let task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
+  }
+
+  findTask(id: string): [number, Task] {
+    const taskIndex = this.tasks.findIndex(task => task.id === id);
+    const task = this.tasks[taskIndex];
+    return [taskIndex, task];
+  }
+}
